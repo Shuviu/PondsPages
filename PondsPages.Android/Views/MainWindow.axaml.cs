8d208bde4ecb07d6614faee5346f1aed76e87981
shuviu <shuviu@proton.me>
2025-06-20 17:19:34 +0000

    Created the base project structure - Split parts into corresponding projects - Created necessary dirs - Create basic classes

---
+++ PondsPages.Android/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace PondsPages.Android.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
