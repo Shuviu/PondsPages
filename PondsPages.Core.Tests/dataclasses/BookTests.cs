36433fb9e6cda003dc8854dd49fd21277156a4e6
shuviu <shuviu@proton.me>
2025-06-22 12:16:08 +0000

    Unit tests for Book Dataclass
    - Added Unit tests for the Book class
    - Fixed minor Bugs in the ISBN Check calculation

--- PondsPages.Core.Tests/dataclasses/BookTests.cs
+++ PondsPages.Core.Tests/dataclasses/BookTests.cs
@@ -1,3 +1,5 @@
+using PondsPages.dataclasses;
+
 namespace PondsPages.Core.Tests.dataclasses;
 
 public class BookTests
@@ -6,31 +8,74 @@ public class BookTests
     public void IsbnCheck_TestValid13Digit_ReturnsTrue()
     {
         // ---- Arrange ---- //
+        const string isbn = "978-0-316-38311-0";
         
         // ---- Act ---- //
+        bool isValid = Book.IsbnCheck(isbn);
         
         // ---- Assert ---- //
+        Assert.True(isValid);
     }
     [Fact]
     public void IsbnCheck_TestInvalid13Digit_ReturnsFalse()
     {
+        // ---- Arrange ---- //
+        const string isbn = "978-3-7416-4175-8";
         
+        // ---- Act ---- //
+        bool isValid = Book.IsbnCheck(isbn);
+        
+        // ---- Assert ---- //
+        Assert.False(isValid);
     }
     [Fact]
     public void IsbnCheck_TestValid10Digit_ReturnsTrue()
     {
+        // ---- Arrange ---- //
+        const string isbn = "0-19-853453-1";
         
+        // ---- Act ---- //
+        bool isValid = Book.IsbnCheck(isbn);
+        
+        // ---- Assert ---- //
+        Assert.True(isValid);
     }
 
     [Fact]
     public void IsbnCheck_TestInvalid10Digit_ReturnsFalse()
     {
+        // ---- Arrange ---- //
+        const string isbn = "0-19-853453-2";
         
+        // ---- Act ---- //
+        bool isValid = Book.IsbnCheck(isbn);
+        
+        // ---- Assert ---- //
+        Assert.False(isValid);
     }
     
     [Fact]
-    public void IsbnCheck_TestInvalid_ReturnsFalse()
+    public void IsbnCheck_TestInvalidChar_ReturnsFalse()
     {
+        // ---- Arrange ---- //
+        const string isbn = "invalid";
         
+        // ---- Act ---- //
+        bool isValid = Book.IsbnCheck(isbn);
+        
+        // ---- Assert ---- //
+        Assert.False(isValid);
+    }
+    [Fact]
+    public void IsbnCheck_TestInvalidLength_ReturnsFalse()
+    {
+        // ---- Arrange ---- //
+        const string isbn = "978-3-7416-41-8";
+        
+        // ---- Act ---- //
+        bool isValid = Book.IsbnCheck(isbn);
+        
+        // ---- Assert ---- //
+        Assert.False(isValid);
     }
 }
